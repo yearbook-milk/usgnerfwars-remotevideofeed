427cd486eb70783f09bf822ba38502e0bff1892f
unknown <throwaway0x069420@gmail.com>
2023-07-20 14:37:00 -0400

    added error chekcing forserver side

--- server/remote.py
+++ server/remote.py
@@ -1,6 +1,6 @@
-import socket
+import socket, time
 
-signaling_port = 10008
+signaling_port = 10007
 data_channel_port = 10009
 TCP_SOCKET = None
 UDP_SOCKET = None
@@ -8,37 +8,54 @@ TCP_REMOTE_PEER = None
 TCP_CONNECTION = None
 
 def readFrom(protocol, sock, bufSize = 1024):
-    assert bufSize > 1
-    if protocol == "TCP":
-        try:
-            return sock.recv(bufSize)
-        except:
-            return None
-    elif protocol == "UDP":
-        assert bufSize < 65535
-        try:
-            return sock.recvFrom(bufSize)
-        except:
-            return None
+    global TCP_REMOTE_PEER
+    try:
+        assert bufSize > 1
+        if protocol == "TCP":
+            try:
+                return sock.recv(bufSize)
+            except:
+                return None
+        elif protocol == "UDP":
+            assert bufSize < 65535
+            try:
+                return sock.recvFrom(bufSize)
+            except:
+                return None
+    except (ConnectionResetError, OSError, BrokenPipeError) as e:
+        print(f"Cx Error {e}! Attempting a reconnect...")
+        initConnection()
 
 
-def sendTo(protocol, sock, message):
-    sock.send(bytes(message))
-    return None
+def sendTo(protocol, sock, message, destiny = None):
+    try:
+        if protocol == "TCP":
+            sock.send(message)
+        else:
+            assert destiny != None
+            sock.sendto(message, (destiny, data_channel_port))
+        return None
+    except (ConnectionResetError, OSError, BrokenPipeError) as e:
+        print(f"Cx Error {e}! Attempting a reconnect...")
+        initConnection()
 
-def setupParameters(tcpport = 10008, udpport = 10009):
+def setupParameters(tcpport = 10007, udpport = 10009):
     global signaling_port, data_channel_port
     signaling_port = tcpport
-    data_channel_port = udppport
+    data_channel_port = udpport
     
 def initConnection():
+    time.sleep(5)
+    print("Waiting to connect again...")
     global TCP_CONNECTION, TCP_SOCKET, UDP_SOCKET, TCP_REMOTE_PEER, signaling_port, data_channel_port
     # if we're breaking up with the current pair, we close() the sockets in preparation for a nwe partner
     if TCP_SOCKET:
         TCP_SOCKET.close()
         TCP_SOCKET = None
+        print("Closed TCP socket.")
     if UDP_SOCKET:
         UDP_SOCKET = None
+        print("Closed UDP socket.")
 
     TCP_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # the program will block until someone else connects to the thing
@@ -46,7 +63,7 @@ def initConnection():
     # get a better name
 
     TCP_SOCKET.bind(("0.0.0.0", signaling_port))
-    print(f"<signalerTCP> Listening on TCP port {signaling_port}")
+    print(f"Listening on TCP port {signaling_port}")
     TCP_SOCKET.listen()
     while True:
         try:
@@ -54,12 +71,16 @@ def initConnection():
             break
         except:
             # do nothing, because there is no connection
+            pass
 
-    print(f"<signalerTCP> Remote peer has connected: {addr}")
+    print(f"Remote peer has connected: {addr}")
 
     # now that everything is set up, we can set global objects that can be read from
     TCP_CONNECTION = conn
-    TCP_CONNECTION.sendTo("TCP", TCP_SOCKET, str(data_channel_port))
+    TCP_CONNECTION.send(bytes(str(data_channel_port), "ascii"))
     TCP_REMOTE_PEER = addr
     UDP_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    UDP_SOCKET.bind( (addr[0], 10009) )
+    UDP_SOCKET.setblocking(0)
+    print("Success!")
+    # UDP_SOCKET.bind( (addr[0], 10009) )
+    
--- server/testUDP.py
+++ server/testUDP.py
@@ -2,16 +2,18 @@ import remote, time, random
 
 remote.setupParameters()
 try: remote.initConnection()
-except Exception as e:
+except ZeroDivisionError as e:
     print(f"Could not establish connection with other peer: {e}")
 
 
 while True:
-    msg = bytes(str(random.randint(0,1000)))
-    remote.sendTo("UDP", remote.UDP_SOCKET, msg)
+    msg = bytes(str(random.randint(0,1000)), "ascii")
+    remote.sendTo("UDP", remote.UDP_SOCKET, msg, remote.TCP_REMOTE_PEER[0])
     print(f"I just sent something to the remote ({msg})")
-    time.sleep(0.5)
 
-    r = readFrom("TCP", remote.TCP_CONNECTION, 2048):
+    r = remote.readFrom("TCP", remote.TCP_CONNECTION, 2048)
     if r:
         print(f"The remote just sent something to me: {r}")
+        # 
+
+    time.sleep(0.5)

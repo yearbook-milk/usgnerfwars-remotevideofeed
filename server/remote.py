b162e55502d1655847b5f8585e094d4acb4fb8e7
unknown <throwaway0x069420@gmail.com>
2023-07-27 23:29:44 -0400

    modified server/remote.py to reflect issue changes in main repo

--- server/remote.py
+++ server/remote.py
@@ -14,7 +14,7 @@ def readFrom(protocol, sock, bufSize = 1024):
         if protocol == "TCP":
             try:
                 return sock.recv(bufSize)
-            except:
+            except Exception as e:
                 return None
         elif protocol == "UDP":
             assert bufSize < 65535
@@ -23,21 +23,21 @@ def readFrom(protocol, sock, bufSize = 1024):
             except:
                 return None
     except (ConnectionResetError, OSError, BrokenPipeError) as e:
-        print(f"Cx Error {e}! Attempting a reconnect...")
-        initConnection()
+        print(f"[net] Cx Error {e}! Attempting a reconnect...")
+        #initConnection()
 
 
 def sendTo(protocol, sock, message, destiny = None):
     try:
         if protocol == "TCP":
-            sock.send(message)
+            sock.sendall(message)
         else:
             assert destiny != None
             sock.sendto(message, (destiny, data_channel_port))
         return None
     except (ConnectionResetError, OSError, BrokenPipeError) as e:
-        print(f"Cx Error {e}! Attempting a reconnect...")
-        initConnection()
+        print(f"[net] Cx Error {e}! Attempting a reconnect...")
+        #initConnection()
 
 def setupParameters(tcpport = 10007, udpport = 10009):
     global signaling_port, data_channel_port
@@ -46,16 +46,16 @@ def setupParameters(tcpport = 10007, udpport = 10009):
     
 def initConnection():
     time.sleep(5)
-    print("Waiting to connect again...")
+    print("[net] Waiting to connect again...")
     global TCP_CONNECTION, TCP_SOCKET, UDP_SOCKET, TCP_REMOTE_PEER, signaling_port, data_channel_port
     # if we're breaking up with the current pair, we close() the sockets in preparation for a nwe partner
     if TCP_SOCKET:
         TCP_SOCKET.close()
         TCP_SOCKET = None
-        print("Closed TCP socket.")
+        print("[net] Closed TCP socket.")
     if UDP_SOCKET:
         UDP_SOCKET = None
-        print("Closed UDP socket.")
+        print("[net] Closed UDP socket.")
 
     TCP_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # the program will block until someone else connects to the thing
@@ -63,7 +63,7 @@ def initConnection():
     # get a better name
 
     TCP_SOCKET.bind(("0.0.0.0", signaling_port))
-    print(f"Listening on TCP port {signaling_port}")
+    print(f"[net] Listening on TCP port {signaling_port}")
     TCP_SOCKET.listen()
     while True:
         try:
@@ -73,14 +73,15 @@ def initConnection():
             # do nothing, because there is no connection
             pass
 
-    print(f"Remote peer has connected: {addr}")
+    print(f"[net] Remote peer has connected: {addr}")
 
     # now that everything is set up, we can set global objects that can be read from
     TCP_CONNECTION = conn
+    TCP_CONNECTION.setblocking(0)
     TCP_CONNECTION.send(bytes(str(data_channel_port), "ascii"))
     TCP_REMOTE_PEER = addr
     UDP_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     UDP_SOCKET.setblocking(0)
-    print("Success!")
+    print("[net] Success!")
     # UDP_SOCKET.bind( (addr[0], 10009) )
     
